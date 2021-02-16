33ce05ee09a54d50b9514fe783c552edbcd3dc9f
khoabui <buikhoa793@gmail.com>
2021-02-16 21:27:07 +0200

    deploy

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express();
+const path = require("path");
+const PORT = 8001;
+
+// app.get("/", (req, res) => {
+//     res.send("server");
+// })
+
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+app.listen(PORT, () => {
+    console.log('Server listening on port ${PORT}');
+})
